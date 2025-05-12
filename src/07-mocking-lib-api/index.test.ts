3d7752eacbee2206826882c7eb446415de6bae69
ismoilkhon <ismoilkhon.bakhromov@mail.ru>
2025-05-12 17:20:17 +0500

    feat: add mokking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,54 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  throttle: <T extends (...args: unknown[]) => unknown>(fn: T): T => fn,
+}));
 
 describe('throttledGetDataFromApi', () => {
+  const mockData = { id: 1, title: 'test' };
+  const relativePath = '/test';
+
+  let mockGet: jest.Mock;
+  let axiosCreateSpy: jest.SpyInstance<AxiosInstance>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    mockGet = jest.fn().mockResolvedValue({ data: mockData });
+
+    (axios.create as jest.Mock).mockImplementation(() => ({
+      get: mockGet,
+    }));
+
+    axiosCreateSpy = jest.spyOn(axios, 'create');
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axiosCreateSpy).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi(relativePath);
+
+    expect(mockGet).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const result = await throttledGetDataFromApi(relativePath);
+
+    expect(result).toEqual(mockData);
+  });
+
+  test('should throw if request fails', async () => {
+    const error = new Error('Network Error');
+    mockGet.mockRejectedValueOnce(error);
+
+    await expect(throttledGetDataFromApi(relativePath)).rejects.toThrow(error);
   });
 });
